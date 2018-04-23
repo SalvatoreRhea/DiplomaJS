36a1b7aa69ca0c4083e878deec27316c6d3b4213
Salvatore <salvatoredl@mail.ru>
2018-04-23 14:42:06 +0800

    DiplomaJS

--- js/script.js
+++ js/script.js
@@ -115,6 +115,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
             // Добавляем биографию в карточку
             newBio.innerHTML   = candidate_bio.value;
+            newBio.style.overflowWrap = "break-word";
 
             // Добавляем картинку из custom в карточку
             newPhoto.style.backgroundImage = personEasy.style.backgroundImage;
